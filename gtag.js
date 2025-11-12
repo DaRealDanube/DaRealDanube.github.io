ebd4645f8136d012a302361d9563184f3b40dc61
Danube Mappings <penquinkitten13@gmail.com>
2025-11-12 06:40:01 -0600

    Add Google Analytics tracking script

---
+++ gtag.js
@@ -0,0 +1,14 @@
+const GA_ID = 'G-EV05SH93DZ';
+
+// the gtag.js script tag
+const gaScript = document.createElement('script');
+gaScript.async = true;
+gaScript.src = `https://www.googletagmanager.com/gtag/js?id=${G-EV05SH93DZ}`;
+document.head.appendChild(gaScript);
+
+// Initialize Google Analytics
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+
+gtag('js', new Date());
+gtag('config', G-EV05SH93DZ);
